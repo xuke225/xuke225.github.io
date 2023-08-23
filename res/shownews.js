f6be04534b0cfc9fc8db5eb313f3c10c7f02f897
xuke225 <xuke@ahu.edu.cn>
2023-08-23 17:09:10 +0800

    new blog

---
+++ res/shownews.js
@@ -0,0 +1,13 @@
+function showNews() {
+    var x = document.getElementById("hidden_news");
+    x.style.display = "block";
+    var x = document.getElementById("hide_news_botton");
+    x.style.display = "none";
+}
+
+function showEvents() {
+    var x = document.getElementById("hidden_events");
+    x.style.display = "block";
+    var x = document.getElementById("hide_events_botton");
+    x.style.display = "none";
+}
